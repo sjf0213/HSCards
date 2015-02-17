6c05525d8461c6bb41efea96de1a0bfe3c0879d2
sjf0213 <sjf0213@163.com>
2015-02-17 10:34:53 +0800

    create project

---
+++ HSCards/HSCards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HSCards
+//
+//  Created by song jufeng on 15/2/17.
+//  Copyright (c) 2015年 song jufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HSCards/HSCards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HSCards
+//
+//  Created by song jufeng on 15/2/17.
+//  Copyright (c) 2015年 song jufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
