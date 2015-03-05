ff00ab45c34814de74e2153dd2e54950f76129be
sjf0213 <sjf0213@163.com>
2015-03-05 10:47:02 +0800

    optimize filter

--- HSCards/HSCards/CardItemInfo.h
+++ HSCards/HSCards/CardItemInfo.h
@@ -12,18 +12,18 @@
 
 @property(nonatomic, readonly)NSString* cardID;// 代码
 @property(nonatomic, readonly)NSString* cardName;// 名称
+@property(nonatomic, readonly)BOOL collectible;// 是否衍生卡牌
+
 @property(nonatomic, readonly)NSUInteger cost;// 费用，法力值消耗
+@property(nonatomic, readonly)NSUInteger attack;// 攻击力（随从和武器牌才有，法术牌没有）
+@property(nonatomic, readonly)NSUInteger health;// 生命值（随从牌专属）
+@property(nonatomic, readonly)NSUInteger durability;// 耐久度（武器牌专属）
+
 @property(nonatomic, readonly)NSString* rarity;// 稀有程度
 @property(nonatomic, readonly)NSString* cardType;// 卡牌类型（随从，法术，武器，英雄，英雄技能，XXX）
 @property(nonatomic, readonly)NSString* career;// 所属职业
 @property(nonatomic, readonly)NSString* race;// 所属种族：鱼人，机械等
-@property(nonatomic, readonly)BOOL collectible;// 是否衍生卡牌
-
-@property(nonatomic, readonly)NSUInteger atk;// 攻击力（随从和武器牌才有，法术牌没有）
-@property(nonatomic, readonly)NSUInteger health;// 生命值（随从和武器牌才有，法术牌没有）
-@property(nonatomic, readonly)NSUInteger durability;// 耐久度（武器牌专属）
-
-@property(nonatomic, readonly)NSString*  cardSet;// 卡牌来源集合（纳克萨玛斯等）
+@property(nonatomic, readonly)NSString* cardSet;// 卡牌来源集合（纳克萨玛斯等）
 
 -(id)initWithDic:(NSDictionary*)dic;
 
--- HSCards/HSCards/CardItemInfo.m
+++ HSCards/HSCards/CardItemInfo.m
@@ -9,22 +9,21 @@
 #import "CardItemInfo.h"
 @interface CardItemInfo()
 @property(nonatomic, strong)NSDictionary* otherInfo;
+
 @property(nonatomic, strong)NSString* cardName;
 @property(nonatomic, strong)NSString* cardID;
-@property(nonatomic, assign)NSUInteger cost;
-@property(nonatomic, assign)NSString* rarity;
+@property(nonatomic, assign)BOOL collectible;// 是否衍生卡牌
+
+@property(nonatomic, assign)NSUInteger cost;// 费用，法力值消耗
+@property(nonatomic, assign)NSUInteger attack;// 攻击力（随从和武器牌才有，法术牌没有）
+@property(nonatomic, assign)NSUInteger health;// 生命值（随从牌专属）
+@property(nonatomic, assign)NSUInteger durability;// 耐久度（武器牌专属）
+
+@property(nonatomic, assign)NSString* rarity;// 稀有程度
 @property(nonatomic, assign)NSString* cardType;// 卡牌类型（随从，法术，武器，英雄，英雄技能，XXX）
 @property(nonatomic, assign)NSString* career;// 所属职业
 @property(nonatomic, assign)NSString* race;// 所属种族
-@property(nonatomic, assign)BOOL collectible;// 是否衍生卡牌
-@property(nonatomic, assign)NSString*  cardSet;
-
-@property(nonatomic, assign)NSUInteger attack;// 攻击力（随从和武器牌才有，法术牌没有）
-@property(nonatomic, assign)NSUInteger health;// 生命值（随从和武器牌才有，法术牌没有）
-@property(nonatomic, assign)NSUInteger durability;// 耐久度（武器牌专属）
-
-
-
+@property(nonatomic, assign)NSString* cardSet;//卡牌所属集合
 @end
 @implementation CardItemInfo
 
@@ -34,7 +33,6 @@
     if(self)
     {
         NSNumber* num = nil;
-        NSString* str  = nil;
         _otherInfo = dic;
         
         // 卡牌代码
@@ -43,22 +41,35 @@
         // 卡牌名称
         _cardName = [dic objectForKey:@"name"];
         
+        // 是否可收集
+        num = [dic objectForKey:@"collectible"];
+        if ([num isKindOfClass:[NSNumber class]]) {
+            _collectible = [num boolValue];
+        }
+        
         // 费用，法力值消耗
         num = [dic objectForKey:@"cost"];
         if ([num isKindOfClass:[NSNumber class]]) {
             _cost = [num unsignedIntegerValue];
         }
+        // 攻击力
+        num = [dic objectForKey:@"attack"];
+        if ([num isKindOfClass:[NSNumber class]]) {
+            _attack = [num unsignedIntegerValue];
+        }
+        // 随从生命值
+        num = [dic objectForKey:@"health"];
+        if ([num isKindOfClass:[NSNumber class]]) {
+            _health = [num unsignedIntegerValue];
+        }
+        // 武器耐久度
+        num = [dic objectForKey:@"durability"];
+        if ([num isKindOfClass:[NSNumber class]]) {
+            _durability = [num unsignedIntegerValue];
+        }
         
         // 稀有程度
         _rarity = [dic objectForKey:@"rarity"];
-        
-        // 职业
-        str = [dic objectForKey:@"playerClass"];
-        if ([str isKindOfClass:[NSString class]]) {
-            _career = str;
-        }else{
-            _career = @"Neutral"; // 默认值为中立
-        }
         
         // 种族
         _race = [dic objectForKey:@"race"];
@@ -66,10 +77,12 @@
         // 卡牌类型
         _cardType = [dic objectForKey:@"type"];
         
-        // 是否可收集
-        num = [dic objectForKey:@"collectible"];
-        if ([num isKindOfClass:[NSNumber class]]) {
-            _collectible = [num boolValue];
+        // 职业
+        NSString* str = [dic objectForKey:@"playerClass"];
+        if ([str isKindOfClass:[NSString class]]) {
+            _career = str;
+        }else{
+            _career = @"Neutral"; // 默认值为中立
         }
     }
     return self;
